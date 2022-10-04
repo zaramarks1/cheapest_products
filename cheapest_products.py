90759290d6d85395ec20616c9d5090de81c7994a
Zara Marks <zara.marks@reseau.eseo.fr>
2022-10-04 11:06:49 -0500

    errors with finding class fixed

--- cheapest_products.py
+++ cheapest_products.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 import pandas as pd
+from geopy.geocoders import Nominatim
+import geocoder
+import re
 
 products = []
 prices = []
@@ -12,7 +15,8 @@ def set_up(product):
 
     #SET UP SELENIUM
 
-    url_walmart = f'https://www.walmart.com/search?q={product}'
+    url_walmart = f'https://www.walmart.com/search?q={product}&sort=price_low'
+    url_target = f'https://www.target.com/s?searchTerm={product}&sortBy=PriceLow&moveTo=product-list-grid'
 
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
@@ -23,35 +27,38 @@ def set_up(product):
     "prefs", {"profile.managed_default_content_settings.images": 2}
 )
 
+    my_location= geocoder.ip('me')
+    latitude= my_location.geojson['features'][0]['properties']['lat']
+    longitude = my_location.geojson['features'][0]['properties']['lng']
+
+    # latitude = 36.169941
+    # longitude = -115.139832
+
+    print(latitude, longitude)
 
     browser = webdriver.Chrome('/Users/zaramarks/Desktop/Zara/prog/drivers/chromedriver', chrome_options=options)
 
-
+  #  browser.execute_cdp_cmd("Emulation.setGeolocationOverride", {
+   #         "latitude": latitude,
+    #        "longitude": longitude,
+    #        "accuracy": 100
+   # })
 
     browser.get(url_walmart)
 
-
     soap = BeautifulSoup(browser.page_source, 'lxml')
 
-    return soap
+    transform(soap)
 
-def extract_target(product):
-
-    url_target = f'https://www.target.com/s?searchTerm={product}&sortBy=PriceLow&moveTo=product-list-grid'
-
-    url_walmart = f'https://www.walmart.com/search?q={product}'
-
-    response = requests.get(url_walmart)
-
-    soap = BeautifulSoup(response.text, 'lxml')
     return soap
 
 def transform(soap):
    
-   #Walmart redering
-    products.append(soap.find_all('a', class_ = 'absolute w-100 h-100 z-1 hide-sibling-opacity', limit=1)[0].find('span', class_ = 'w_Be').string)
+    #Walmart redering
+    #products.append(soap.find_all('a', class_ = 'absolute w-100 h-100 z-1 hide-sibling-opacity', limit =1)[0].find('span', class_ = 'w_Be').string)
+    products.append(soap.find_all('a', class_ = 'absolute w-100 h-100 z-1 hide-sibling-opacity', limit =1)[0].find('span').string)
     prices.append( soap.find('div', class_='mr1 mr2-xl lh-copy b black f5 f4-l').string.replace('$', ''))
-    ratings.append(soap.find('div', class_= 'flex items-center mt2').find('span', class_='w_Be').string)
+    ratings.append(soap.find('div', class_= 'flex items-center mt2').find('span', string = re.compile('Stars')).string)
     stores.append('Walmart')
 
     df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings, 'Store':stores})
@@ -60,4 +67,4 @@ def transform(soap):
 
 
 c = set_up('pencil+sharpener')
-transform(c)
+
